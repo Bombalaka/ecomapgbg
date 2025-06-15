0915b203ad149657a09c3ee3f00d0b6eb04534a2
Bombalaka <yotaka.johansson@hotmail.com>
2025-06-15 11:27:32 +0200

    Add site.css and update styles for improved UI
    - Added site.css to the project for additional styling.
    - Updated static web asset configurations to include site.css.
    - Modified existing CSS styles for body, titles, buttons, and cards for better aesthetics and responsiveness.
    - Enhanced hover effects for buttons and cards to improve user interaction.
    - Adjusted layout properties for better alignment and spacing.

--- EcoMapGbg/obj/Debug/net9.0/EcoMapGbg.AssemblyInfo.cs
+++ EcoMapGbg/obj/Debug/net9.0/EcoMapGbg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoMapGbg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8cf6579fab2a26bacb3ced19c97cf2fe8dc866")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e24a2fd4a99a5ac4ecf2ad23c6d0ddf6a413578")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoMapGbg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoMapGbg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
